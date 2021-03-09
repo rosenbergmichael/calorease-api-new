972ba56c303fefd7d6e68295c6b374ab01748dc4
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-03-09 15:19:31 -0500

    build out Trackers and Foods controllers

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -1,2 +1,48 @@
 class Api::V1::FoodsController < ApplicationController
+  before_action :set_tracker
+
+  def index
+    @foods = @tracker.foods 
+    render json: @foods 
+  end
+
+
+  def create
+    @food = @tracker.foods.new(food_params)
+    @tracker.update_cal(@food)
+    if @food.save
+      render json: @tracker 
+    else
+      render json: {error: 'Error, please try again.'}
+    end
+  end
+
+
+  def show
+    @food = Food.find(params[:id])
+    render json: @food 
+  end
+
+
+  def destroy
+    @food = Food.find(params["id"])
+    @tracker = Tracker.find(@food.tracker_id)
+    @tracker.delete_food(@food)
+    @food.destroy
+    render json: @tracker 
+  end
+
+  
+  private
+
+  def set_tracker
+    @tracker = Tracker.find(params[:tracker_id])
+  end
+
+
+  def food_params
+    params.require(:food).permit(:tracker_id, :name, :calories)
+  end
+
+
 end
--- app/controllers/api/v1/trackers_controller.rb
+++ app/controllers/api/v1/trackers_controller.rb
@@ -1,2 +1,39 @@
 class Api::V1::TrackersController < ApplicationController
+  def index
+    @trackers = Tracker.all 
+    render json: @trackers
+  end
+
+
+  def create
+    @tracker = Tracker.new(tracker_params)
+    if @tracker.save
+      render json: @tracker 
+    else
+      render json: {error: 'Error, please try again.'}
+    end
+  end
+
+
+  def show
+    @tracker = Tracker.find(params[:id])
+    render json: @tracker
+  end
+
+
+  def destroy
+    @tracker = Tracker.find(params[:id])
+    @tracker.destroy
+  end
+
+  
+  private
+
+  def tracker_params
+    params.require(:tracker).permit(:day, :date, :totalcal)
+  end
+
+
 end
+
+
