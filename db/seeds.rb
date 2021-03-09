05b7c583b1d87b517b7321b3e15c77a16b811f80
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-03-09 16:07:46 -0500

    add active_model_serializers gem to Gemfile, bundle i

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# create_table "foods", force: :cascade do |t|
+#   t.integer "tracker_id"
+#   t.string "name"
+#   t.float "calories"
+#   t.datetime "created_at", precision: 6, null: false
+#   t.datetime "updated_at", precision: 6, null: false
+# end
+
+# create_table "trackers", force: :cascade do |t|
+#   t.string "day"
+#   t.date "date"
+#   t.float "totalcal"
+#   t.datetime "created_at", precision: 6, null: false
+#   t.datetime "updated_at", precision: 6, null: false
+# end
+
+tracker = Tracker.create(day: "Monday", date: Date.today, totalcal: 0)
+
+food = Food.create(tracker_id: 1, name: "apple", calories: 50)
+
+food_two = Food.create(tracker_id: 1, name: "pizza", calories: 300)
